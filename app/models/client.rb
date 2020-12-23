e32795f0d46ba4b19bb9d6e70bbce7b121319237
jennifer tracy <jenniferetracy@gmail.com>
2020-12-23 18:08:31 -0500

    trying methods

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,14 @@
-class Client < ActiveRecord::Base
+class Client <ActiveRecord::Base
     has_many :rentals
     has_many :vhs, through: :rentals
-end
+
+    def current_rentals
+        Rental.all.select{|rental| rental.client_id == self.id}
+    end
+
+    def return_all
+        binding.pry 
+        self.current_rentals.current = false
+    end 
+
+end    
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,14 @@
 class Rental < ActiveRecord::Base 
     belongs_to :client 
     belongs_to :vhs 
+
+    def due_date
+        d = self.created_at.to_date
+        d + 7
+    end    
 end 
+
+# d.to_s
+# => "2009-08-31"
+# >> (d+3).to_s
+# => "2009-09-03"
