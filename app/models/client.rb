e75b021040dc2f31f8478feaf0678a01f48e59a6
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2020-12-24 13:07:34 -0500

    1224vhs

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,13 +5,25 @@ class Client <ActiveRecord::Base
     def self.first_rental(name, home_address, vhs)
         new_client = Client.new(name: name, home_address: home_address)
         Rental.create(vhs_id: vhs.id, current: true, client_id: new_client.id)
-    end    
-    def current_rentals
-        Rental.all.select{|rental| rental.client_id == self.id}
-    end
+    end  
+
+    # def current_rentals
+    #     Rental.all.where{|rental| rental.client_id == self.id}
+    # end
 
     def return_all
-        self.current_rentals.current = false
+        binding.pry
+        self.rentals.where(vhs_id: vhs.ids).update(current: false) 
     end 
 
+    def number_of_rentals 
+        self.rentals.count
+    end 
+
+    def self.paid_most 
+        self 
+        place_holder = 0
+        self.number_of_rentals
+        binding.pry
+    end
 end    
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,11 @@ class Movie < ActiveRecord::Base
     has_many :vhs 
     has_many :movie_genres 
     has_many :genres, through: :movie_genres
+
+    def report_stolen 
+        binding.pry
+        
+        self.vhs.rentals.current(current: false) 
+        self.vhs.random.delete
+    end 
 end 
